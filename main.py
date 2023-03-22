00f9edec27cffde798f5465e3db566b17e943f8a
Pedro Henrique <>
2023-03-22 00:44:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+#operacçoes basicas
+#num1 = 10
+#num2 = 20
+#agregaçao
+#n1 = '3'
+#n2 = '2'
+#total = n1+ n2 
+
+#print(total)
+
+#num1 =int(input('insira um número'))
+#num2 = int (input('insira o segundo número'))
+#total = num1 + num2 
+
+#print(f"o valor da soma é{total}")
+
+#exemplo salario 
+#salario = int (input("insira seu salario:"))
+
+#if salario <= 1000:
+#  aumento = salario + (salario*0.2)
+#  print(f"salario sofreu um aumento de 20% totalizando{aumento}")
+#elif salario >= 1000: #Aumento de 10%
+  #aumento  = salario + (salario*0.1)
+ # print(f"salario sofreu um aumento de 10% totalizando{aumento}")
+#Boletim 
+
+#nota1 =int(input('insira a nota: '))
+#nota2 =int(input('insira a nota: '))
+#media = (nota1 + nota2)/2 
+#if media >= 6: #aprovado
+ # print(f"aprovado, sua nota é {media}")
+#else: #reprovado
+ # print (f"reprovado, sua nota é {media}")
+
+#nome = input("insira seu nome: ")
+#idade =int(input("insira sua idade "))
+#if idade  >= 18:
+ # print(f"ola {nome } voce é maior com {idade} anos")
+#else: 
+ # print(f"ola {nome } voce é menor com {idade} anos")
+
+#tabuada  = int (input("qual tabuada voce eseja :"))
+#cont = 9
+#soma= 0
+#while cont != 0:
+  #soma = tabuada * cont 
+  #print(f"{tabuada} * { cont} = {soma}")
+  #cont = cont -1
+#parada=""
+#while True: 
+  #Fruta = input ("insirao nome de uma fruta :")
+  ##parada = input ("deseja continuars/n:"). upper()
+  #if parada != "s":
+   #breakpoint
+
+
+parada=""
+while True: 
+  name = input ("Digite um nome: ")
+  idade = int(input("Digite á idade: "))
+  cidade = input("digite sua cidade: ")
+  parada = input ("deseja continuar  S/N:"). UPPER()
+  if parada !="s":
+    break
+  
